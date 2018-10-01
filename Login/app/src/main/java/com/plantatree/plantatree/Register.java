9441a62d5f13d1cb35b26d95bd094b8960828caf
Pierce <Ath1oncq60>
2018-10-01 20:15:27 +1300

    fixmistake_2.0

--- Login/app/src/main/java/com/plantatree/plantatree/Register.java
+++ Login/app/src/main/java/com/plantatree/plantatree/Register.java
@@ -1,7 +1,8 @@
 package com.plantatree.plantatree;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,46 +12,53 @@ import com.stream53.plantatree.plantatree.R;
 
 public class Register extends AppCompatActivity {
 
-    DatabaseHelper db;
-    EditText EMAIL, PASS, CONFIRM;
-    Button REGISTER;
-
+    DatabaseHelper DATABASE;
+    EditText EMAIL, PASSWORD, CONFIRM_PASS;
+    Button REGISTER, LOGIN_REG;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        DATABASE = new DatabaseHelper(this);
+        EMAIL=(EditText)findViewById(R.id.edit_text_emailReg);
+        PASSWORD =(EditText)findViewById(R.id.edit_text_regPass);
+        CONFIRM_PASS =(EditText)findViewById(R.id.edit_text_confirm);
+        REGISTER=(Button)findViewById(R.id.button_register);
+        LOGIN_REG=(Button)findViewById(R.id.button_regLogin);
 
-        db = new DatabaseHelper(this);
+        //LOGIN BUTTON, ON REGISTER ACTIVITY LISTENER
+        LOGIN_REG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Register.this, Login.class);
+                startActivity(i);
+            }
+        });
 
-        EMAIL = (EditText)findViewById(R.id.edit_text_email);
-        PASS = (EditText)findViewById(R.id.edit_text_pass);
-        CONFIRM = (EditText)findViewById(R.id.edit_text_confirm);
-        REGISTER = (Button)findViewById(R.id.button_register);
+        //REGISTER BUTTON, ON REGISTER ACTIVITY LISTENER
         REGISTER.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                String grab_email = EMAIL.getText().toString();
-                String grab_pass = PASS.getText().toString();
-                String grab_confirm = CONFIRM.getText().toString();
-
-                if(grab_email.equals("")||grab_pass.equals("")||grab_confirm.equals("")){
-                    Toast.makeText(getApplicationContext(), "Whats up?",Toast.LENGTH_SHORT).show();
-                }else{
-                    if(grab_pass.equals(grab_confirm)){
-                        Boolean validateEmail = db.validateEmail(grab_email);
-                        if(validateEmail==true){
-                            Boolean insert = db.addUser(grab_email, grab_pass);
+                String s1 = EMAIL.getText().toString();
+                String s2 = PASSWORD.getText().toString();
+                String s3 = CONFIRM_PASS.getText().toString();
+                if(s1.equals("")||s2.equals("")||s3.equals("")){
+                    Toast.makeText(getApplicationContext(), "Fields are empty",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(s2.equals(s3)){
+                        Boolean chkemail = DATABASE.validateEmail(s1);
+                        if(chkemail==true){
+                            Boolean insert = DATABASE.addUser(s1,s2);
                             if(insert==true){
-                                Toast.makeText(getApplicationContext(), "Chur Bro", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "Registered Successfully",Toast.LENGTH_SHORT).show();
                             }
-                        }else{
-                            Toast.makeText(getApplicationContext(), "We've got that email my bro", Toast.LENGTH_SHORT).show();
                         }
-
+                        else{
+                            Toast.makeText(getApplicationContext(), "Email Already exists", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
-
             }
         });
     }
