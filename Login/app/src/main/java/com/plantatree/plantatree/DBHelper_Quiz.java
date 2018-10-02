d72c5bcd5aa027e04cf20dd1909b730a91ba2b5c
Pierce <Ath1oncq60>
2018-10-02 16:04:02 +1300

    quiz_questions.update

--- Login/app/src/main/java/com/plantatree/plantatree/DBHelper_Quiz.java
+++ Login/app/src/main/java/com/plantatree/plantatree/DBHelper_Quiz.java
@@ -47,34 +47,44 @@ public class DBHelper_Quiz extends SQLiteOpenHelper {
 
     public void fillQuestionsTable() {
 
-        Quiz_Question q1 = new Quiz_Question("You need to help your children maintain a healthy balanced diet. Which of these foods is high calcium?", "Fish", "Peanuts", "Yogurt", 3);
+        Quiz_Question q1 = new Quiz_Question("Trees may help reduce the effects of global warming by:",
+                "Reduce CO2 in the atmosphere", "Provide shade from UV rays", "Increase oxygen in atmosphere", 1);
         addQuestion(q1);
 
-        Quiz_Question q2 = new Quiz_Question("When do children start learning speech patterns?", "Between 1-2 years old", "At three months", "In the womb", 3);
+        Quiz_Question q2 = new Quiz_Question("What two main substances do trees need to live?",
+                "They absorb CO2 and H2O", "Trees absorb oxygen", "Trees only need sunlight", 1);
         addQuestion(q2);
 
-        Quiz_Question q3 = new Quiz_Question("Which of these foods, high in potassium?", "Peas", "Whole Grain Bread", "Bananas", 3);
+        Quiz_Question q3 = new Quiz_Question("The amount of CO2 a tree may remove from the atmosphere to gain one pound is:",
+                "About 3 pounds", "About 0.5 pounds", "About 1.5 pounds", 3);
         addQuestion(q3);
 
-        Quiz_Question q4 = new Quiz_Question("Babies move around and wiggle so much because...", "They're exploring", "They're uncomfortable", "Their muscles twitch and flex", 1);
+        Quiz_Question q4 = new Quiz_Question("What is one of the byproducts of photosynthesis?",
+                "Shade", "Oxygen", "Shade", 2);
         addQuestion(q4);
 
-        Quiz_Question q5 = new Quiz_Question("What street do Grover, Elmo, Kermit and Big Bird live on?", "23rd", "Sesame", "Mr. Roger's", 2);
+        Quiz_Question q5 = new Quiz_Question("The water vapor given off by a tree becomes a critical part of Earth's ______________.",
+                "Cloud cycle", "Hydrologic cycle", "Beauty", 2);
         addQuestion(q5);
 
-        Quiz_Question q6 = new Quiz_Question("True or False: You should make a child vomit if you suspect he or she is suffering from poison.", "It depends what type of poison", "False", "True", 2);
+        Quiz_Question q6 = new Quiz_Question("What element is used from the absorbed water to make the chemical energy for a tree?.",
+                "Oxygen", "Carbon Dioxide", "Hydrogen", 3);
         addQuestion(q6);
 
-        Quiz_Question q7 = new Quiz_Question("Fill in the blank: 'Hush little baby don’t say a word, Papa’s gonna buy you a _________", "Mockingbird", "Firebird", "Horse", 1);
+        Quiz_Question q7 = new Quiz_Question("Trees are sometimes called ...?", "The Lungs of the World",
+                "The Stomach of the World", "The Shady Part of the World", 1);
         addQuestion(q7);
 
-        Quiz_Question q8 = new Quiz_Question("Which is generally considered better - breastfeeding or bottle feeding?", "Breastfeeding", "Bottle feeding", "Neither is considered", 1);
+        Quiz_Question q8 = new Quiz_Question("Approximately how much of the original forests have disappeared?",
+                "3/4", "1/2", "1/3", 2);
         addQuestion(q8);
 
-        Quiz_Question q9 = new Quiz_Question("Most babies get their first tooth when they are how old?", "3 months", "6 months", "1 year", 2);
+        Quiz_Question q9 = new Quiz_Question("The oldest living organism in North America happens to be a rugged tree. This tree is the?",
+                "Bristlecone Pine", "Douglas Fir", "Giant Redwood", 1);
         addQuestion(q9);
 
-        Quiz_Question q10 = new Quiz_Question("What should your baby's first solid food be?", "Seafood", "Soft cooked veges and fruit", "Whatever the parents eat", 2);
+        Quiz_Question q10 = new Quiz_Question("What is the science of dating through tree-ring growth called?",
+                "Dendrochronology", "Eldology", "Lookingology", 1);
         addQuestion(q10);
     }
 
