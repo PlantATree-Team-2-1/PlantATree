03ad13a4a7f6155a15e9479f96143316aacdecf7
Mark Berney <berneydwmark@gmail.com>
2018-10-11 12:39:02 +1300

    Revert "clean up file system"
    This reverts commit a03ab12be28f0cb173e3cde6c04b25edd19d622b.

---
+++ app/app/src/main/java/com/stream53/plantatree/plantatree/Login.java
@@ -0,0 +1,41 @@
+package com.stream53.plantatree.plantatree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText EMAIL_LOGIN, PASS_LOGIN;
+    Button LOGIN;
+    DatabaseHelper DATABASE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        DATABASE = new DatabaseHelper(this);
+        EMAIL_LOGIN = (EditText)findViewById(R.id.edit_text_emailLogin);
+        PASS_LOGIN = (EditText)findViewById(R.id.edit_text_loginPass);
+        LOGIN = (Button)findViewById(R.id.button_login);
+
+        //LOGIN BUTTON, ON LOGIN ACTIVITY LISTENER
+        LOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = EMAIL_LOGIN.getText().toString();
+                String password = PASS_LOGIN.getText().toString();
+                Boolean Chkemailpass = DATABASE.emailPassword(email, password);
+                if(Chkemailpass==true){
+                    Toast.makeText(getApplicationContext(), "Successfully Login", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong email or Passowrd", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
